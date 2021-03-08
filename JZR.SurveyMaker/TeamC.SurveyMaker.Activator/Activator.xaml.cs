813e7745e1968324bc91077857768065b91804c1
Jason Ryan <300057524@fvtc.edu>
2021-03-08 16:15:55 -0600

    Setup the Activator project to communicate with the API

--- JZR.SurveyMaker/TeamC.SurveyMaker.Activator/Activator.xaml.cs
+++ JZR.SurveyMaker/TeamC.SurveyMaker.Activator/Activator.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using JZR.SurveyMaker.BL.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,9 +21,43 @@ namespace TeamC.SurveyMaker.Activator
     /// </summary>
     public partial class Activator : Window
     {
+        List<Question> questions;
+
         public Activator()
         {
             InitializeComponent();
+
+            questions = new List<Question>();
+
+            Reload();
+        }
+
+        private void Reload()
+        {
+            HttpClient client = InitializeClient();
+            HttpResponseMessage response;
+            string result;
+            dynamic items;
+
+            response = client.GetAsync("Question").Result;
+            result = response.Content.ReadAsStringAsync().Result;
+            items = (JArray)JsonConvert.DeserializeObject(result);
+            questions = items.ToObject<List<Question>>();
+
+            Rebind();
+        }
+
+        private void Rebind()
+        {
+            dgvQuestions.ItemsSource = null;
+            dgvQuestions.ItemsSource = questions;
+        }
+
+        private HttpClient InitializeClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44327/api/");
+            return client;
         }
     }
 }
