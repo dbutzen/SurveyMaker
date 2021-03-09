abb0473bc7357937a5e25d29b4e2ce2981b86fa5
Jason Ryan <300057524@fvtc.edu>
2021-03-08 20:07:23 -0600

    Undid changes in Activator.

--- JZR.SurveyMaker/TeamC.SurveyMaker.Activator/Activator.xaml.cs
+++ JZR.SurveyMaker/TeamC.SurveyMaker.Activator/Activator.xaml.cs
@@ -1,5 +1,4 @@
-﻿using JZR.SurveyMaker.BL;
-using JZR.SurveyMaker.BL.Models;
+﻿using JZR.SurveyMaker.BL.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
@@ -33,25 +32,32 @@ namespace TeamC.SurveyMaker.Activator
             Reload();
         }
 
-        private async  void Reload()
+        private void Reload()
         {
+            HttpClient client = InitializeClient();
+            HttpResponseMessage response;
+            string result;
+            dynamic items;
 
-            questions = await QuestionManager.Load();
+            response = client.GetAsync("Question").Result;
+            result = response.Content.ReadAsStringAsync().Result;
+            items = (JArray)JsonConvert.DeserializeObject(result);
+            questions = items.ToObject<List<Question>>();
 
             Rebind();
         }
 
         private void Rebind()
         {
-            //cboQuestions.ItemsSource = null;
-            //cboQuestions.ItemsSource = questions;
+            //dgvQuestions.ItemsSource = null;
+            //dgvQuestions.ItemsSource = questions;
         }
 
-        //private HttpClient InitializeClient()
-        //{
-        //    var client = new HttpClient();
-        //    client.BaseAddress = new Uri("https://localhost:44327/api/");
-        //    return client;
-        //}
+        private HttpClient InitializeClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44327/api/");
+            return client;
+        }
     }
 }
