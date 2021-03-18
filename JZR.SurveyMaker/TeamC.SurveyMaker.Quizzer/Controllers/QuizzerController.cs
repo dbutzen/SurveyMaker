552b237fb388b539e703480dfbfcf64ce11782c8
Daniel Butzen <300057079@fvtc.edu>
2021-03-18 16:36:31 -0400

    Quizzer web should be working

--- JZR.SurveyMaker/TeamC.SurveyMaker.Quizzer/Controllers/QuizzerController.cs
+++ JZR.SurveyMaker/TeamC.SurveyMaker.Quizzer/Controllers/QuizzerController.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using JZR.SurveyMaker.BL.Models;
+using TeamC.SurveyMaker.Quizzer.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace TeamC.SurveyMaker.Quizzer.Controllers
 {
@@ -20,6 +24,58 @@ namespace TeamC.SurveyMaker.Quizzer.Controllers
         // GET: QuizzerController
         public ActionResult Index()
         {
+            try
+            {
+                string activationCode = Request.Form["txtActivationCode"].ToString();
+                QuizzerViewModel.activationCode = activationCode;
+
+
+                HttpClient client = InitializeClient();
+                HttpResponseMessage response;
+                string result;
+                dynamic items;
+
+                Question question = null;
+                QuizzerViewModel quizzerViewModel = new QuizzerViewModel();
+
+                response = client.GetAsync("Question/" + activationCode).Result;
+
+                result = response.Content.ReadAsStringAsync().Result;
+                items = (JObject)JsonConvert.DeserializeObject(result);
+                question = items.ToObject<Question>();
+
+
+                QuizzerViewModel.question = question;
+
+                return View(question);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Error with activation code. Code might not exists.");
+            }
+            
+            
+
+        }
+
+        public ActionResult Submit()
+        {
+            string strAnswerId = Request.Form["selectedButton"];
+
+            Response response = new Response();
+            response.Id = Guid.NewGuid();
+            response.QuestionId = QuizzerViewModel.question.Id;
+            response.Answers = QuizzerViewModel.question.Answers;
+            response.ResponseDate = DateTime.Now;
+            response.AnswerId = new Guid(strAnswerId);
+
+            var client = InitializeClient();
+            var serializedObject = JsonConvert.SerializeObject(response);
+            var content = new StringContent(serializedObject);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            var postResponse = client.PostAsync("Response?rollback=true", content).Result;
+
             return View();
         }
 
---
+++ JZR.SurveyMaker/TeamC.SurveyMaker.Quizzer/Models/QuizzerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using JZR.SurveyMaker.BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamC.SurveyMaker.Quizzer.Models
+{
+    public class QuizzerViewModel
+    {
+        public static string activationCode { get; set; }
+        public static Question question { get; set; }
+
+    }
+}
